6b3995a1cbb2ecc260beb47ca0c3de29fd18d5fb
Arthur Cruz <espindola.art@gmail.com>
2023-08-29 14:50:15 -0300

    66, 69

--- Easy/PlusOne_66.java
+++ Easy/PlusOne_66.java
@@ -1,74 +1,36 @@
+import java.util.Arrays;
+
 public class PlusOne_66 {
 
     public static void main(String[] args) {
-        int[] array1 = { 1, 2, 3 };
-        plusOne(array1);
-        int[] array2 = { 4, 3, 2, 1 };
-        plusOne(array2);
-        int[] array3 = { 9 };
-        plusOne(array3);
-
+        // int[] array1 = { 1, 2, 3 };
+        // plusOne(array1);
+        // int[] array2 = { 4, 3, 2, 1 };
+        // plusOne(array2);
+        // int[] array3 = { 9 };
+        // plusOne(array3);
+        int[] array4 = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+        plusOne(array4);
     }
 
     public static int[] plusOne(int[] digits) {
 
-        int tam = digits.length;
-        int numElev = tam - 1;
-        int soma = 0;
+        int carry = 1;
 
-        for (int i = 0; i < digits.length; i++) {
-            soma += digits[i] * (int) Math.pow(10, numElev);
-            numElev--;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            int sum = digits[i] + carry;
+            digits[i] = sum % 10; // Valor a ser mantido no dígito
+            carry = sum / 10; // Valor a ser carregado para o próximo dígito
         }
 
-        soma++;
-
-        String numeroString = Integer.toString(soma);
-
-        int quantidadeDigitos = numeroString.length();
-
-        if (tam < quantidadeDigitos) {
-            int[] novoArray = new int[tam + 1];
-            for (int i = 0; i < quantidadeDigitos; i++) {
-                novoArray[i] =  Character.getNumericValue(numeroString.charAt(i));
-            }
-
-            for (int i = 0; i < quantidadeDigitos; i++) {
-                System.out.println(novoArray[i]);
-            }
-
-            return novoArray;
+        if (carry > 0) {
+            int[] newDigits = new int[digits.length + 1];
+            newDigits[0] = carry;
+            System.arraycopy(digits, 0, newDigits, 1, digits.length);
+            return newDigits;
         } else {
-            for (int i = 0; i < quantidadeDigitos; i++) {
-                digits[i] = Character.getNumericValue(numeroString.charAt(i));
-            }
-
-            for (int i = 0; i < quantidadeDigitos; i++) {
-                System.out.println(digits[i]);
-            }
-
             return digits;
         }
-
-        // int i = 0;
-        // while (i <= digits.length - 1 && digits[digits.length - 1 - i] == 9) {
-        //     i++;
-        // }
-
-        // int[] output = (digits.length == i) ? new int[digits.length + 1] : new int[digits.length];
-
-        // if (digits.length == i) {
-        //     output[0] = 1;
-        // } else {
-
-        //     for (int j = 0; j <= digits.length - i - 1; j++) 
-        //     {
-        //         output[j] = digits[j];
-        //     }
-
-        //     output[digits.length - 1 - i] = digits[digits.length - i - 1] + 1;
-        // }
-        // return output;
 
     }
 
